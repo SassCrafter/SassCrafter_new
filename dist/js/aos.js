37ba088a1d44a8ca502aa38205fc8bc29d4bfbb7
Dima <dima@gmail.com>
2020-11-14 09:10:57 +0200

    Added AOS animation and vendor prefixes

---
+++ dist/js/aos.js
@@ -0,0 +1,11 @@
+AOS.init({
+    offset: 250,
+    delay: 150,
+    easing: "ease-in-out-cubic",
+    mirror: true,
+});
+
+//const cd_content = document.querySelectorAll(".cd-section-content");
+//cd_content.forEach((el) => {
+  //  el.setAttribute("data-aos", "fade-up");
+//});
