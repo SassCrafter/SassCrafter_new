b778f87b9a1d910e3f3f9b68ad51eb5e748436be
Dima <dima@gmail.com>
2020-11-15 13:42:37 +0200

    Add portfolio page and first project

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,5 +2,15 @@ window.onload = function() {
     let loader = document.querySelector(".preloader");
     setTimeout(function() {
         loader.parentNode.removeChild(loader);
-    }, 3500);
+    }, 100);
 }
+
+const cards = document.querySelectorAll(".projects-card");
+cards.forEach(el => {
+    el.addEventListener("click", function() {
+        cards.forEach(el => {
+            el.classList.remove("projects-card-clicked");
+        });
+        el.classList.add("projects-card-clicked");
+    });
+});
