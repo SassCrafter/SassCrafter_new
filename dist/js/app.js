de47ee126603ebe485b4b0beee28d92d621b2872
Dima <dima@gmail.com>
2020-11-16 19:51:36 +0200

    Added fullpage js

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,6 +5,33 @@ window.onload = function() {
     }, 3000);
 }
 
+
+// Fullpage
+let myFullPage = new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollingSpeed: 1400,
+    controlArrows: true,
+    loopTop: true,
+    loopBottom: true,
+    parallax: true,
+    parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+});
+
+// Controlls
+const arrow_top = document.getElementById("arrow-top");
+const arrow_bot = document.getElementById("arrow-down");
+
+arrow_top.addEventListener("click", function() {
+    arrow_top.classList.add("hi");
+    fullpage_api.moveSectionUp();
+});
+arrow_bot.addEventListener("click", function() {
+    fullpage_api.moveSectionDown();
+});
+
+
+
+
 const cards = document.querySelectorAll(".projects-card");
 cards.forEach(el => {
     el.addEventListener("click", function() {
